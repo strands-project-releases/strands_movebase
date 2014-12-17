5768700dbdf20ebb6f2407f54108dd7e350c1e48
Chris Burbridge <cburbridge@gmail.com>
2014-12-17 15:35:57 +0000

    Rebase from 'upstream'

--- strands_movebase/src/subsample_cloud.cpp
+++ strands_movebase/src/subsample_cloud.cpp
@@ -8,60 +8,68 @@
 #include "strands_movebase/noise_approximate_voxel_grid.h"
 
 ros::Publisher pub;
+double resolution;
+int min_points;
+int skip_points;
 
 void callback(const sensor_msgs::PointCloud2::ConstPtr& msg)
 {
     pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>());
     pcl::fromROSMsg(*msg, *cloud);
-	pcl::PointCloud<pcl::PointXYZ> voxel_cloud;
-	
-    noise_approximate_voxel_grid sor(5, 20); // a bit faster than nvg, not as accurate
-	sor.setInputCloud(cloud);
-    sor.setLeafSize(0.05f, 0.05f, 0.05f);
-	sor.filter(voxel_cloud);
+    pcl::PointCloud<pcl::PointXYZ> voxel_cloud;
 
-	/*pcl::StatisticalOutlierRemoval<pcl::PointXYZ> sor; // another possibility, slow
-	sor.setInputCloud(new_cloud);
-	sor.setMeanK(20);
-	sor.setStddevMulThresh(1.0);
-	sor.filter(voxel_cloud);*/
+    noise_approximate_voxel_grid sor(min_points, skip_points); // a bit faster than nvg, not as accurate
+    sor.setInputCloud(cloud);
+    sor.setLeafSize(resolution, resolution, resolution);
+    sor.filter(voxel_cloud);
 
-	sensor_msgs::PointCloud2 msg_cloud;
+    /*pcl::StatisticalOutlierRemoval<pcl::PointXYZ> sor; // another possibility, slow
+    sor.setInputCloud(new_cloud);
+    sor.setMeanK(20);
+    sor.setStddevMulThresh(1.0);
+    sor.filter(voxel_cloud);*/
+
+    sensor_msgs::PointCloud2 msg_cloud;
     pcl::toROSMsg(voxel_cloud, msg_cloud);
-	msg_cloud.header = msg->header;
+    msg_cloud.header = msg->header;
 
-	pub.publish(msg_cloud);
+    pub.publish(msg_cloud);
 }
 
 int main(int argc, char** argv)
 {
     ros::init(argc, argv, "subsample_cloud");
-	ros::NodeHandle n;
-	
+    ros::NodeHandle n;
+
+    ros::NodeHandle pn("~");
     // topic of input cloud
-    if (!n.hasParam("/subsample_cloud/input")) {
+    if (!pn.hasParam("input")) {
         ROS_ERROR("Could not find parameter input.");
         return -1;
     }
     std::string input;
-    n.getParam("/subsample_cloud/input", input);
-    
+    pn.getParam("input", input);
+
     // topic of output cloud
-    if (!n.hasParam("/subsample_cloud/output")) {
+    if (!pn.hasParam("output")) {
         ROS_ERROR("Could not find parameter output.");
         return -1;
     }
     std::string output;
-    n.getParam("/subsample_cloud/output", output);
-    
-	ros::Subscriber sub = n.subscribe(input, 1, callback);
+    pn.getParam("output", output);
+
+    pn.param<double>("resolution", resolution, 0.05);
+    pn.param<int>("min_points", min_points, 5);
+    pn.param<int>("skip_points", skip_points, 20);
+
+    ros::Subscriber sub = n.subscribe(input, 1, callback);
     pub = n.advertise<sensor_msgs::PointCloud2>(output, 1);
-    
+
     ros::Rate rate(5); // updating at 5 hz, slightly faster than move_base
     while (n.ok()) {
         rate.sleep();
         ros::spinOnce();
     }
-	
-	return 0;
+
+    return 0;
 }
