e13dc7b1880ac36fe885c6638c813624ca6d3c96
STRANDS Jenkins <strands@hanheide.net>
2016-02-08 16:19:39 +0000

    Revert "Trimmed the branch to only the calibrate_chest sub directory"
    This reverts commit 173b78ffba824c6254e970ad11bff19e2f84212d.

---
+++ strands_movebase/src/subsample_cloud.cpp
@@ -0,0 +1,75 @@
+#include <ros/ros.h>
+#include <sensor_msgs/PointCloud2.h>
+#include <pcl_ros/point_cloud.h>
+#include <pcl/point_types.h>
+#include <pcl/filters/voxel_grid.h>
+#include <pcl/filters/statistical_outlier_removal.h>
+#include <boost/thread/thread.hpp>
+#include "strands_movebase/noise_approximate_voxel_grid.h"
+
+ros::Publisher pub;
+double resolution;
+int min_points;
+int skip_points;
+
+void callback(const sensor_msgs::PointCloud2::ConstPtr& msg)
+{
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>());
+    pcl::fromROSMsg(*msg, *cloud);
+    pcl::PointCloud<pcl::PointXYZ> voxel_cloud;
+
+    noise_approximate_voxel_grid sor(min_points, skip_points); // a bit faster than nvg, not as accurate
+    sor.setInputCloud(cloud);
+    sor.setLeafSize(resolution, resolution, resolution);
+    sor.filter(voxel_cloud);
+
+    /*pcl::StatisticalOutlierRemoval<pcl::PointXYZ> sor; // another possibility, slow
+    sor.setInputCloud(new_cloud);
+    sor.setMeanK(20);
+    sor.setStddevMulThresh(1.0);
+    sor.filter(voxel_cloud);*/
+
+    sensor_msgs::PointCloud2 msg_cloud;
+    pcl::toROSMsg(voxel_cloud, msg_cloud);
+    msg_cloud.header = msg->header;
+
+    pub.publish(msg_cloud);
+}
+
+int main(int argc, char** argv)
+{
+    ros::init(argc, argv, "subsample_cloud");
+    ros::NodeHandle n;
+
+    ros::NodeHandle pn("~");
+    // topic of input cloud
+    if (!pn.hasParam("input")) {
+        ROS_ERROR("Could not find parameter input.");
+        return -1;
+    }
+    std::string input;
+    pn.getParam("input", input);
+
+    // topic of output cloud
+    if (!pn.hasParam("output")) {
+        ROS_ERROR("Could not find parameter output.");
+        return -1;
+    }
+    std::string output;
+    pn.getParam("output", output);
+
+    pn.param<double>("resolution", resolution, 0.05);
+    pn.param<int>("min_points", min_points, 5);
+    pn.param<int>("skip_points", skip_points, 20);
+
+    ros::Subscriber sub = n.subscribe(input, 1, callback);
+    pub = n.advertise<sensor_msgs::PointCloud2>(output, 1);
+
+    ros::Rate rate(5); // updating at 5 hz, slightly faster than move_base
+    while (n.ok()) {
+        rate.sleep();
+        ros::spinOnce();
+    }
+
+    return 0;
+}
